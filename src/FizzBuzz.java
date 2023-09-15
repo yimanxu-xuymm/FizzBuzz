48ae54bdad059b9dc66819a203fb6a0189c51899
Yiman Xu <yimanxu@Melody-Xu-MacBook-Pro.local>
2023-09-15 11:38:39 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -10,17 +10,18 @@ class FizzBuzz {
             // Find out which numbers divide i.
             boolean divisibleBy3 = i % 3 == 0;
             boolean divisibleBy5 = i % 5 == 0;
+            doFizzBuzz result = new doFizzBuzz(divisibleBy3, divisibleBy5);
 
             // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
+            if (result.divisibleBy3() && result.divisibleBy5()) {
 
                 System.out.println("Fizz Buzz");
 
-            } else if (divisibleBy3) {
+            } else if (result.divisibleBy3()) {
 
                 System.out.println("Fizz");
 
-            } else if (divisibleBy5) {
+            } else if (result.divisibleBy5()) {
 
                 System.out.println("Buzz");
 
@@ -30,5 +31,8 @@ class FizzBuzz {
 
             }
         }
+    }
+
+    private record doFizzBuzz(boolean divisibleBy3, boolean divisibleBy5) {
     }
 }
